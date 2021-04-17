8a1e022ec72c258771af02b66913c439e2f33428
Rufaidashafiq <rufaidashafik@gmail.com>
2021-04-17 12:45:09 +0500

    setting up comments routes and psql queries

--- server/Main/routes.js
+++ server/Main/routes.js
@@ -49,3 +49,45 @@ router.delete('/api/delete/post', (req, res , next) =>{
 })
 module.exports = router
  
+
+// COMMENTS routes section
+
+router.posts('/api/posts/commenttodb',(req, res, next) =>{
+  const values=[req.body.comment, req.body.user_id, req.body.usernmae, req.body.post_id]
+  pool.query(`INSERT INTO comments(comment,user_id , author, post_id, date_created) VALUES($1,$2,$3, $4, NOW())`, values, (q_err, q_res) =>{
+    res.json(q_res.rows)
+    console.log(q_err)
+  })
+})
+
+router.put('/api/put/commenttodb', (req, res, next) =>{
+  const values=[req.body.comment, req.body.user_id, req.body.post_id , req.body.usernmae,req.body.cid]
+  pool.query(`UPDATE comments SET comment=$1, user_id= $2, post_id =$3, author=$4, date_created=NOW()
+            WHERE cid =$5`, values,
+            (q_err,q_res) =>{
+              res.json(q_res.rows)
+              console.log(q_err)
+            })
+
+})
+
+router.delete('/api/delete/comment' , (req, res ,next) =>{
+  const cid= req.body.cid
+  pool.query(`DELETE FROM comments
+            WHERE cid= $1`, [cid],
+            (q_err, q_res)=>{
+              res.json(q_res.rows)
+              console.log(q_err)
+            })
+})
+
+router.get('/api/get/allpostcomments', (req, res, next) =>{
+  const post_id= String(req.query.post_id)
+  pool.query(`SELECT * FROM comments 
+              WHERE post_id= $1`, [post_id],
+              (q_err, q_res) =>{
+              res.json(q_res, rows)
+              console.log(q_err)
+
+  })
+})
